8d0a9e76872133b4ad86e366b6eaadee83b2ce47
btvoidx <btvoidx@gmail.com>
2021-04-14 20:13:56 +0400

    Added header

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -2,6 +2,15 @@
 	import '../app.postcss';
 </script>
 
+<header class="fixed top-0 w-screen h-16 flex justify-center">
+	<div class="h-full w-full max-w-4xl flex flex-row items-center p-4 space-x-6 text-xl">
+		<a href="/" class="text-2xl mr-4">Мой Блог</a>
+		<a href="/articles">Статьи</a>
+		<a href="/about">Обо мне</a>
+		<a href="/contacts" class="!ml-auto">Связаться</a>
+	</div>
+</header>
+
 <main>
 	<slot />
 </main>
