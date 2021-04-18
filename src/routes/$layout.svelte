972220282a8109db0577edab352b4b54ac6ae6ed
btvoidx <btvoidx@gmail.com>
2021-04-18 20:42:46 +0400

    tweaks

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -2,15 +2,15 @@
 	import '../app.postcss';
 </script>
 
-<header class="fixed top-0 w-screen h-16 flex justify-center">
-	<div class="h-full w-full max-w-4xl flex flex-row items-center p-4 space-x-6 text-xl">
-		<a href="/" class="text-2xl mr-4">Мой Блог</a>
+<header class="fixed top-0 flex justify-center w-screen h-16 bg-white">
+	<div class="flex flex-row items-center w-full h-full max-w-4xl p-4 space-x-6 text-xl">
+		<a href="/" class="mr-4 text-2xl">Мой Блог</a>
 		<a href="/articles">Статьи</a>
 		<a href="/about">Обо мне</a>
 		<a href="/contacts" class="!ml-auto">Связаться</a>
 	</div>
 </header>
 
-<main>
+<main class="flex justify-center w-screen min-h-screen">
 	<slot />
 </main>
