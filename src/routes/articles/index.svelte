b01c214f6610869b79fe9f5573be35f797bdf60b
btvoidx <btvoidx@gmail.com>
2021-04-18 20:43:22 +0400

    Articles page

---
+++ src/routes/articles/index.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import ArticlePreview from '../../components/common/ArticlePreview.svelte';
+</script>
+
+<div class="flex flex-col items-center w-full pt-32 space-y-16">
+	<h1 class="text-5xl">Недавние статьи</h1>
+	<div class="grid max-w-4xl gap-10 w-96 auto-rows-min lg:grid-cols-2 lg:w-full">
+		<ArticlePreview>
+			<h2 class="text-2xl" slot="title">Заголовок 1</h2>
+			<p slot="text">Небольшой отрывок текста...</p>
+		</ArticlePreview>
+		<ArticlePreview>
+			<h2 class="text-2xl" slot="title">Заголовок 2</h2>
+			<p slot="text">Небольшой отрывок другого текста, который не связан с первым...</p>
+		</ArticlePreview>
+		<ArticlePreview />
+		<ArticlePreview />
+		<ArticlePreview />
+		<ArticlePreview />
+		<ArticlePreview />
+		<ArticlePreview />
+		<ArticlePreview />
+	</div>
+</div>
